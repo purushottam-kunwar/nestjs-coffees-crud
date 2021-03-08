ffb2b450441a4c61e428de48b252d1fc6d5e80bb
purushottam <pipalpurus@gmail.com>
2021-03-08 15:09:30 +0545

    added postgres database setup

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,15 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity() // sql table == 'coffee'
 export class Coffee {
+  @PrimaryGeneratedColumn() // for primary key
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
